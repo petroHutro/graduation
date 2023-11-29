9222ad2da8d9fffe4ab26008bbe2810e14484985
petroHutro <artyrxx@list.ru>
2023-11-29 16:49:48 +0300

    fix encoding

--- internal/encoding/encoding.go
+++ internal/encoding/encoding.go
@@ -5,34 +5,21 @@ import (
 	"fmt"
 )
 
-var secretKey = "mySecretKey"
-
 func EncodeID(id int) string {
 	idBytes := []byte(fmt.Sprintf("%d", id))
-
-	encodedBytes := []byte(secretKey)
-	for i := range idBytes {
-		idBytes[i] ^= encodedBytes[i%len(encodedBytes)]
-	}
-
 	return base64.StdEncoding.EncodeToString(idBytes)
 }
 
 func DecodeID(encodedID string) (int, error) {
 	idBytes, err := base64.StdEncoding.DecodeString(encodedID)
 	if err != nil {
-		return 0, fmt.Errorf("cannot Decode String: %v", err)
-	}
-
-	encodedBytes := []byte(secretKey)
-	for i := range idBytes {
-		idBytes[i] ^= encodedBytes[i%len(encodedBytes)]
+		return 0, fmt.Errorf("cannot decode string: %v", err)
 	}
 
 	var decodedID int
 	_, err = fmt.Sscanf(string(idBytes), "%d", &decodedID)
 	if err != nil {
-		return 0, fmt.Errorf("cannot Sscanf: %v", err)
+		return 0, fmt.Errorf("cannot sscanf: %v", err)
 	}
 
 	return decodedID, nil
